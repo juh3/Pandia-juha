6a9fabfec2fca986670f650511a5600b15ef9d1c
Juha <juha.tapio20@gmail.com>
2024-03-07 18:37:54 +0200

    responsivess

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,94 +1,84 @@
-import styles from './Form.module.css'
-import Input from '../Input/Input'
-import { InputType } from '../../types/types'
-import React, { ChangeEventHandler, useState } from 'react'
-import Button from '../Button/Button'
-import { song_options, key_options } from '../../utils/options'
-import { KaraokeSchema } from '../../schemas/KaraokeSchema'
-import { ZodError, ZodIssue } from 'zod'
+import styles from './Form.module.css';
+import Input from '../Input/Input';
+import { InputType } from '../../types/types';
+import React, { ChangeEventHandler, useState } from 'react';
+import Button from '../Button/Button';
+import { song_options, key_options } from '../../utils/options';
+import { KaraokeSchema } from '../../schemas/KaraokeSchema';
+import { ZodError, ZodIssue } from 'zod';
 
 const Form = () => {
-  const [name, setName] = useState('')
-  const [song, setSong] = useState('')
-  const [allowSave, setAllowSave] = useState(false)
-  const [songKey, setSongKey] = useState('0')
-  const [picture, setPicture] = useState<null | File>(null)
-  const [submitting, setSubmitting] = useState(false)
-  const [validationErrors, setValidationErrors] = useState<
-    ZodIssue[] | null
-  >(null)
+  const [name, setName] = useState('');
+  const [song, setSong] = useState('');
+  const [allowSave, setAllowSave] = useState(false);
+  const [songKey, setSongKey] = useState('0');
+  const [picture, setPicture] = useState<null | File>(null);
+  const [submitting, setSubmitting] = useState(false);
+  const [validationErrors, setValidationErrors] = useState<ZodIssue[] | null>(
+    null
+  );
 
-  const handleNameChange: ChangeEventHandler<HTMLInputElement> = (
-    event
-  ) => {
-    setValidationErrors(null)
-    setName(event.target.value)
-  }
+  const handleNameChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setValidationErrors(null);
+    setName(event.target.value);
+  };
 
-  const handleSongChange: ChangeEventHandler<HTMLSelectElement> = (
-    event
-  ) => {
-    setValidationErrors(null)
+  const handleSongChange: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    setValidationErrors(null);
 
-    setSong(event.target.value)
-  }
+    setSong(event.target.value);
+  };
 
-  const handleSaveChange: ChangeEventHandler<
-    HTMLInputElement
-  > = () => {
-    setValidationErrors(null)
-    setAllowSave(!allowSave)
-  }
+  const handleSaveChange: ChangeEventHandler<HTMLInputElement> = () => {
+    setValidationErrors(null);
+    setAllowSave(!allowSave);
+  };
 
-  const handleSongKeyChange: ChangeEventHandler<HTMLInputElement> = (
-    event
-  ) => {
-    setValidationErrors(null)
-    setSongKey(event.target.value)
-  }
+  const handleSongKeyChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setValidationErrors(null);
+    setSongKey(event.target.value);
+  };
 
-  const handleUpload: ChangeEventHandler<HTMLInputElement> = (
-    event
-  ) => {
-    const { files } = event.target
-    const selectedFiles = files as FileList
-    setPicture(selectedFiles?.[0])
-  }
+  const handleUpload: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const { files } = event.target;
+    const selectedFiles = files as FileList;
+    setPicture(selectedFiles?.[0]);
+  };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    setSubmitting(true)
+    event.preventDefault();
+    setSubmitting(true);
     const formData = {
       name,
       song,
       songKey,
       allowSave,
       picture,
-    }
+    };
     try {
-      KaraokeSchema.parse(formData)
+      KaraokeSchema.parse(formData);
       setTimeout(() => {
-        setSubmitting(false)
-        setName('')
-        setSong('')
-        setSongKey('')
-        setPicture(null)
-      }, 3000)
-      window.alert(JSON.stringify(formData))
+        setSubmitting(false);
+        setName('');
+        setSong('');
+        setSongKey('');
+        setPicture(null);
+      }, 3000);
+      window.alert(JSON.stringify(formData));
     } catch (error) {
       if (error instanceof ZodError) {
         setTimeout(() => {
-          setSubmitting(false)
-        }, 1000)
-        setValidationErrors(error.issues)
+          setSubmitting(false);
+        }, 1000);
+        setValidationErrors(error.issues);
       }
     }
-  }
+  };
 
   return (
     <div className={styles.FormContainer}>
       <h3>Ilmoittautumislomake</h3>
-      <form onSubmit={handleSubmit}>
+      <form className={styles.Form} onSubmit={handleSubmit}>
         <Input
           type={InputType.text}
           label={'Nimi tai nimimerkki*'}
@@ -96,9 +86,7 @@ const Form = () => {
           name="name"
           value={name}
           error={
-            validationErrors?.find(
-              (error) => error.path[0] === 'name'
-            )?.message
+            validationErrors?.find((error) => error.path[0] === 'name')?.message
           }
         />
         <Input
@@ -110,9 +98,8 @@ const Form = () => {
           name="picture"
           accept="image/"
           error={
-            validationErrors?.find(
-              (error) => error.path[0] === 'picture'
-            )?.message
+            validationErrors?.find((error) => error.path[0] === 'picture')
+              ?.message
           }
         />
 
@@ -125,9 +112,7 @@ const Form = () => {
           onChange={handleSongChange}
           value={song}
           error={
-            validationErrors?.find(
-              (error) => error.path[0] === 'song'
-            )?.message
+            validationErrors?.find((error) => error.path[0] === 'song')?.message
           }
         />
         <Input
@@ -138,9 +123,8 @@ const Form = () => {
           onChange={handleSongKeyChange}
           options={key_options}
           error={
-            validationErrors?.find(
-              (error) => error.path[0] === 'songKey'
-            )?.message
+            validationErrors?.find((error) => error.path[0] === 'songKey')
+              ?.message
           }
         />
         <Input
@@ -155,16 +139,12 @@ const Form = () => {
             )?.message
           }
         />
-        <Button
-          type="submit"
-          isSubmitting={submitting}
-          disabled={submitting}
-        >
+        <Button type="submit" isSubmitting={submitting} disabled={submitting}>
           Ilmoittaudu
         </Button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
